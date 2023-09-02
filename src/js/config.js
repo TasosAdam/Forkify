1abfb02b9e7dbb10aeb8cee6262c45d863a7488f
Tasos Adam <Tasosadam1991@gmail.com>
2023-09-02 19:59:52 +0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd2874313-cd8e-49d2-960e-dddfbf177bea';
+export const MODAL_CLOSE_SEC = 2.5;
